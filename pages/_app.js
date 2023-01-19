d4f542b6bd0806e8810fbc828fd4d043c03e9db2
giovani94 <giovaniportal@gmail.com>
2023-01-18 22:51:23 -0800

    Upgraded to Next-13 and added better SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,41 @@
 import { Analytics } from "@vercel/analytics/react";
+import { NextSeo } from "next-seo";
 import React from "react";
-
 import { Layout } from "../components";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
+			<NextSeo
+				title='MaddGamer'
+				titleTemplate='MaddGamer'
+				defaultTitle='MaddGamer'
+				description='Covering all the latest gaming news!'
+				canonical='https://www.maddgamer.com/'
+				openGraph={{
+					type: "website",
+					locale: "en_IE",
+					siteName: "MaddGamer",
+					url: "https://www.maddgamer.com/",
+					title: "MaddGamer",
+					description: "Covering all the latest gaming news!",
+					images: [
+						{
+							url: "/public/favicon.ico",
+							width: 500,
+							height: 500,
+							alt: "MaddGamer",
+						},
+					],
+					siteName: "MaddGamer",
+				}}
+				twitter={{
+					handle: "@maddgamernews",
+					site: "@maddgamernews",
+					cardType: "summary_large_image",
+				}}
+			/>
 			<Component {...pageProps} />
 			<Analytics />
 		</Layout>
