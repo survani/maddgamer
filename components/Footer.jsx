8deb12cb92c7c0dbf78574030a125c6cae0e8a83
giovani94 <giovaniportal@gmail.com>
2023-01-19 23:14:08 -0800

    Fixed footer to have attr and new favicon

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,12 +4,23 @@ const Footer = () => {
 	return (
 		<footer class='p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800'>
 			<span class='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
-				© 2023{" "}
+				© 2023
 				<a href='https://flowbite.com/' class='hover:underline'>
 					MaddGamer
 				</a>
-				. All rights reserved. No part of this website and its content may be
-				reproduced without the permission of the copyright holder.
+				. All rights reserved.
+			</span>
+			<span className='text-sm text-gray-500 dark:text-gray-400'>
+				<a
+					target='_blank'
+					href='https://icons8.com/icon/cT2fRGs6A-PG/pubg-helmet'
+				>
+					Pubg Helmet
+				</a>{" "}
+				icon by{" "}
+				<a target='_blank' href='https://icons8.com'>
+					Icons8
+				</a>
 			</span>
 			<ul class='flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0 list-none'>
 				<li>
