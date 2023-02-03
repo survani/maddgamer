11de83ffa7786c94e69cbaeac38279a025fbd525
giovani94 <giovaniportal@gmail.com>
2023-02-03 11:29:42 -0800

    Added new cache data

---
+++ cache/data.js
@@ -0,0 +1,85 @@
+export const posts = [
+	{
+		slug: "ashika-island-new-map-looks-amazingx",
+		frontMatter: {
+			title: "Ashika Island New Map Looks Amazing",
+			description:
+				"The all new Resurgence map called Ashika Island looks stunning and Beautiful",
+			image:
+				"https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/blog/hero/mw-wz/MWII-S02-WZ2-POIS-TOUT.jpg",
+			date: "2023-02-02T16:56:47+06:00",
+			featured: true,
+			postOfTheMonth: false,
+			author: "Robert Britt",
+			categories: ["News"],
+			tags: ["Warzone 2", "DMZ", "Reveal", "PlayStation", "PC", "Xbox"],
+		},
+	},
+	{
+		slug: "mw2-and-warzone-20-all-season-2-updatesx",
+		frontMatter: {
+			title: "MW2 and Warzone 2.0 All Season 2 Updates",
+			description:
+				"MaddGamer covers all the updates released and soon-to-be-released updates in Season 2. Stay updated with Season 2 of the Call of Duty franchise.",
+			image: "/images/blog/warzone.webp",
+			date: "2023-01-20T16:56:47+06:00",
+			featured: true,
+			postOfTheMonth: false,
+			author: "Robert Britt",
+			categories: ["Articles"],
+			tags: ["Modern Warfare 2", "Warzone 2", "DMZ"],
+		},
+	},
+	{
+		slug: "mw2-and-warzone-20-season-2-delayed-plus-morex",
+		frontMatter: {
+			title: "MW2 and Warzone 2.0 Season 2 Delayed Plus More",
+			description:
+				"MW2 and Warzone 2.0 are due for a new season. Sadly, the delay will push many of the new addition to the game back a few weeks. Let's explore what will be added to Season 2.",
+			image: "/images/blog/warzone2.0.webp",
+			date: "2023-01-19T16:56:47+06:00",
+			featured: false,
+			postOfTheMonth: false,
+			author: "Alexander Hipp",
+			categories: ["News"],
+			tags: [
+				"Warzone 2",
+				"Modern Warfare 2",
+				"DMZ",
+				"PlayStation",
+				"PC",
+				"Xbox",
+			],
+		},
+	},
+	{
+		slug: "our-most-anticipated-shooter-games-list-2023x",
+		frontMatter: {
+			title: "Our Most Anticipated Shooter Games List for 2023",
+			description:
+				"Our very own list of Anticipated Shooter Games in 2023. Discover your next favorite game. [ Updated Daily ]",
+			image: "/images/blog/MDG-MOST-ANTICIPATED.webp",
+			date: "2023-02-03T16:56:47+06:00",
+			featured: true,
+			postOfTheMonth: false,
+			author: "Robert Britt",
+			categories: ["Guides"],
+			tags: ["PlayStation", "PC", "Xbox"],
+		},
+	},
+	{
+		slug: "the-quickest-death-in-warzone-2x",
+		frontMatter: {
+			title: "The Quickest Death in Warzone 2.0",
+			description:
+				"See how a player in Warzone 2.0 dies before even hitting the ground.",
+			image: "/images/blog/warzone2-1.webp",
+			date: "2023-02-01T16:56:47+06:00",
+			featured: true,
+			postOfTheMonth: true,
+			author: "Alexander Hipp",
+			categories: ["News"],
+			tags: ["Warzone 2", "Clips", "PlayStation", "PC", "Xbox"],
+		},
+	},
+];
